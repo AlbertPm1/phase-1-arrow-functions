aa6ca26a3cfc20fef2c33f8363649ccf597fb6ac
Albertpm1 <petermburunganga98@gmail.com>
2022-11-29 10:40:46 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+//declare a function divide 
+// divide 2000 by 100
+function divide(a, b){
+    return 2000 / 100
+
+}
+//have a parameter a and then multiplied it by itself
+const square=(a) => a * a ;
+//created parameter a and b and then added them together
+const add=(a,b) => a + b ;
+add();
